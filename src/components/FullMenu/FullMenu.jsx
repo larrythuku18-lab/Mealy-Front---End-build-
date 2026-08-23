a9f1f337ff8bc09fb5142b0fb0e97122cbf55712
gaitano <dev.gaitano@gmail.com>
2026-08-23 07:04:13 +0300

    feat(full-menu): add select dropdown elements for sorting
    - Adds rating and price sorting elements
    - Adds lucide-react for picker icons

--- src/components/FullMenu/FullMenu.jsx
+++ src/components/FullMenu/FullMenu.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { meals, categories } from "../../data/mockData";
 import "./FullMenu.css";
 import MealOptionCard from "../MealOptionCard/MealOptionCard";
+import { ChevronDown } from "lucide-react";
 
 function FullMenu() {
   const [activeCategory, setActiveCategory] = useState("All");
@@ -16,16 +17,36 @@ function FullMenu() {
     <section className="full-menu">
       <h2>Full Menu</h2>
       <div>
-        <div className="category-bar">
-          {categories.map((cat) => (
-            <button
-              key={cat.id}
-              className={`category-btn ${activeCategory === cat.name ? "active" : ""}`}
-              onClick={() => setActiveCategory(cat.name)}
-            >
-              {cat.name}
-            </button>
-          ))}
+        <div>
+          <div className="category-bar">
+            {categories.map((cat) => (
+              <button
+                key={cat.id}
+                className={`category-btn ${activeCategory === cat.name ? "active" : ""}`}
+                onClick={() => setActiveCategory(cat.name)}
+              >
+                {cat.name}
+              </button>
+            ))}
+          </div>
+          <div className="sort-bar">
+            <div className="select-wrapper">
+              <select>
+                <option>Rating</option>
+                <option>Ascending</option>
+                <option>Descending</option>
+              </select>
+              <ChevronDown className="select-icon" />
+            </div>
+            <div className="select-wrapper">
+              <select>
+                <option>Price</option>
+                <option>Ascending</option>
+                <option>Descending</option>
+              </select>
+              <ChevronDown className="select-icon" />
+            </div>
+          </div>
         </div>
         <div className="search-bar">
           <input placeholder="Search meals..." />
